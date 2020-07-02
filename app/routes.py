8ac6e6c1066b192c064e6e245df0f16da75d17cc
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-02 19:21:11 +0530

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user={'username':'sneha'}
+    posts=[
+        {
+            'author':{'username':'sg1'},
+            'body':'day1'
+        },{
+            'author':{'username':'sg2'},
+            'body':'day2'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
