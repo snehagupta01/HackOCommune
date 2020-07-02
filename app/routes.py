085ed6bdab38c893ae7a794f793aa4e68824ea91
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-02 22:38:31 +0530

    webforms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,3 +16,11 @@ def index():
         }
     ]
     return render_template('index.html',title='Home',user=user,posts=posts)
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requseted for user={} ,remember_me={}'.format(form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',title='Sign In',form=form)
