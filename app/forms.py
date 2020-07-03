c47201e83c1ef943b65d10956f7fcef993ee7357
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-03 19:29:13 +0530

    pagination

--- app/forms.py
+++ app/forms.py
@@ -46,3 +46,10 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
