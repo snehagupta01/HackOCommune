3cfcb6d310d782d466e1f65a148c4a634ab97e29
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-04 15:21:30 +0530

    designing

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +17,8 @@ migrate=Migrate(app,db)
 login=LoginManager(app)
 login.login_view='login'
 mail=Mail(app)   
+bootstrap=Bootstrap(app)
+moment=Moment(app)
 
 from app import routes,models,errors
 
