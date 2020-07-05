11203bac351d714c9fbc1617d5f593b8a58bf948
snehagupta01 <sneha2001gupta@gmail.com>
2020-07-05 16:30:19 +0530

    followers error fixed

--- app/main/routes.py
+++ app/main/routes.py
@@ -92,14 +92,14 @@ def follow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash(_('User %(username)s not found.', username=username))
+            flash('User %(username)s not found.', username=username,'danger')
             return redirect(url_for('main.index'))
         if user == current_user:
-            flash(_('You cannot follow yourself!'))
+            flash('You cannot follow yourself!','warning')
             return redirect(url_for('main.user', username=username))
         current_user.follow(user)
         db.session.commit()
-        flash(_('You are following %(username)s!', username=username))
+        flash('You are following %(username)s!', username=username,'success')
         return redirect(url_for('main.user', username=username))
     else:
         return redirect(url_for('main.index'))
@@ -112,14 +112,14 @@ def unfollow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash(_('User %(username)s not found.', username=username))
+            flash('User %(username)s not found.', username=username,'danger')
             return redirect(url_for('main.index'))
         if user == current_user:
             flash(_('You cannot unfollow yourself!'))
             return redirect(url_for('main.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
-        flash(_('You are not following %(username)s.', username=username))
+        flash('You are not following %(username)s.', username=username,'info')
         return redirect(url_for('main.user', username=username))
     else:
         return redirect(url_for('main.index'))
